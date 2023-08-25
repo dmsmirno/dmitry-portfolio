8f4aa92b44655e5ec71df32a9be759453f74edbe
Dmitry Smirnov <dmitrysmi010@gmail.com>
2023-08-25 14:18:41 -0700

    Created template for navbar component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import { About, Header, Footer, Skills, Work } from './container';
+import { Navbar } from './components';
 import React from 'react';
 
 function App() {
