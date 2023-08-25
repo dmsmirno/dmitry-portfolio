04a76bfa018287bb9f3099c61fc8f58f6187330f
Dmitry Smirnov <dmitrysmi010@gmail.com>
2023-08-25 16:10:21 -0700

    Changed scss import in App.js and className for main div from App to app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 import { About, Header, Footer, Skills, Work } from './container';
 import { Navbar } from './components';
 import React from 'react';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <Navbar />
       <Header />
       <About />
