459a4433d14a3176360e668adc4e15146235c1d9
Dmitry Smirnov <dmitrysmi010@gmail.com>
2023-08-25 14:10:32 -0700

    Added containers to App.js for display

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,20 +6,11 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>cookie</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      <Footer />
     </div>
   );
 }
